280de8c18bfa1ecf52e27b5269833f6644199fac
Vlatko Stojkoski <stojvlatko@gmail.com>
2022-11-19 14:38:00 +0100

    543

---
+++ 543_paketi/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = 1; i <= a; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+			cout << i << endl;
+	}
+
+	return 0;
+}
